84b0fd6c156dfe21b1754245cd71c27a9399f1ed
Gerardo Garavito <garavitogerardo@gmail.com>
2024-10-16 23:54:45 -0600

    placeholder: update sd png and add soundcloud

--- src/app/missinggate/constants/sd_cards.ts
+++ src/app/missinggate/constants/sd_cards.ts
@@ -5,19 +5,23 @@ export interface SD_CARDS_TYPE {
 
 export const sd_cards: SD_CARDS_TYPE[] = [
   {
-    title: 'SPOTIFY',
+    title: 'spotify',
     url: 'https://open.spotify.com/intl-es/artist/6fTOBo7oOruZWGmbYyJmDt',
   },
   {
-    title: 'APPLE_MUSIC',
+    title: 'apple_music',
     url: 'https://music.apple.com/mx/artist/placeholder/1633531745',
   },
   {
-    title: 'YOUTUBE',
+    title: 'youtube',
     url: 'https://www.youtube.com/watch?v=kmfFvFkxVmY',
   },
   {
-    title: 'EMAIL',
-    url: 'mailto:garavitogerardo@gmail.com',
+    title: 'email',
+    url: 'mailto:placeholdercuh@gmail.com',
+  },
+  {
+    title: 'soundcloud',
+    url: 'https://soundcloud.com/missing-gate',
   },
 ]
